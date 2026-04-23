361a6d9356c53a08985a19b33f11262f6c78d307
hoanghieu011 <163662495+hoanghieu011@users.noreply.github.com>
2026-04-23 13:44:18 +0700

    Update ConnectionStrings.cs

--- api/Common/ConnectionStrings.cs
+++ api/Common/ConnectionStrings.cs
@@ -4,6 +4,7 @@ namespace API.Common
 {
     public class ConnectionStrings
     {
+        // public string DefaultConnection { get; set; } = "server=10.30.31.177;port=3306;database=his_common;user=user;password=123456";
         public string DefaultConnection { get; set; } = "server=10.30.29.16;port=3306;database=his_common;user=qlbv;password=#QLbenhvien!2026hnm;AllowPublicKeyRetrieval=True;";
         public MySqlServerVersion MySqlVersion { get; set; } = new MySqlServerVersion(new Version(8, 0, 21));
     }
