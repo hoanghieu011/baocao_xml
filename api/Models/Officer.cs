e53358124d448a89af1e2657f9e9c20f72035116
Trong Tran <trongtrannn.ptit@outlook.com>
2026-04-25 14:55:41 +0700

    fix lỗi

--- api/Models/Officer.cs
+++ api/Models/Officer.cs
@@ -14,18 +14,18 @@ namespace API.Models
         public string officer_name { get; set; }
         public DateTime? ngaysinh { get; set; }
         public int? gioitinh { get; set; }
-        public string? diachi { get; set; }
-        public string? phone { get; set; }
-        public int? cccd { get; set; }
-        public string? email { get; set; }
-        public string? chucdanh { get; set; }
+        //public string? diachi { get; set; }
+        //public string? phone { get; set; }
+        //public int? cccd { get; set; }
+        //public string? email { get; set; }
+        //public string? chucdanh { get; set; }
         public int? status { get; set; }
         public int? csytid { get; set; }
         public DateTime? created_datetime { get; set; }
         public int? officer_type { get; set; }
-        public string? hoc_ham { get; set; }
-        public string? hoc_vi { get; set; }
-        public string? chuc_danh { get; set; }
+        //public string? hoc_ham { get; set; }
+        //public string? hoc_vi { get; set; }
+        //public string? chuc_danh { get; set; }
         public string? ma_bac_si { get; set; }
         public int? khoaid { get; set; }
     }
